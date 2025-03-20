4f392072cccfca21dfa5e2abcc60104494c03a05
Shirin183 <rumayarumiya@gmail.com>
2025-03-20 09:22:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let string = prompt("string typedagi malumot kiriting")
+console.log(string);
+let number = prompt("number typedagi malumot kiriting")
+console.log(number);
+let boolean = prompt("boolean typedagi malumot kiriting")
+console.log(boolean);
+let c = confirm("hamma malumotlarni to'g'ri kiritganizga ishonchingiz komilmi")
+let a = alert(`siz kiritgan ${string} sozi string tybega tegishli 
+siz kiritgan ${number} sozi number tybega tegishli   
+siz kiritgan ${boolean} sozi boolean tybega tegishli   `)
